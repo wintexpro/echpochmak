03a2149364bd7fa80110be6ce47e74404b587e47
Marat Chiraev <rtstaler@yandex.ru>
2020-11-05 19:07:06 +0700

    Fix tests

--- tests/example.test.js
+++ tests/example.test.js
@@ -23,6 +23,16 @@ describe('Asserts', () => {
       './tests/contract/9_PiggyBank_Stranger.tvc',
       './tests/contract/9_PiggyBank_Stranger.abi.json'
     );
+    manager.loadContract(
+      './tests/contract/15_MessageReceiver.tvc',
+      './tests/contract/15_MessageReceiver.abi.json'
+    );
+    manager.loadContract(
+      './tests/contract/15_MessageSender.tvc',
+      './tests/contract/15_MessageSender.abi.json'
+    );
+    await manager.contracts['15_MessageReceiver'].DeployContract();
+    await manager.contracts['15_MessageSender'].DeployContract();
     await manager.contracts['InitParams'].DeployContract();
     await manager.contracts['9_PiggyBank_Owner'].DeployContract();
     await manager.contracts['9_PiggyBank_Stranger'].DeployContract();
@@ -33,13 +43,23 @@ describe('Asserts', () => {
   });
 
   it('test one', async () => {
-    await manager.contracts['9_PiggyBank_Owner'].RunContract('addToDeposit', {
+    await manager.contracts['15_MessageSender'].RunContract('sendMessage', {
+      anotherContract: manager.contracts['15_MessageReceiver'].address,
+    });
+    manager.GiveToAddress(manager.contracts['15_MessageReceiver'].address);
+    let res = await manager.contracts['15_MessageReceiver'].RunContract(
+      'getCounter',
+      {}
+    );
+
+    /*
+        await manager.contracts['9_PiggyBank_Owner'].RunContract('addToDeposit', {
       bankAddress: manager.contracts['9_PiggyBank'].address,
       amount: 100000,
     });
     console.log('AddDeposit 1000');
-    manager.GiveToAddress(manager.contracts['9_PiggyBank'].address);
     let res = await manager.contracts['9_PiggyBank'].RunContract('getData', {});
+     */
     console.log(res);
   });
 });
